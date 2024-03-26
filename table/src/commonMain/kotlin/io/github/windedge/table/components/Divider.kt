e69752269992478513ab1461c984288225b72968
windedge <windedge99@gmail.com>
2024-03-26 02:27:22 +0100

    remove material-3 dependency

--- table/src/commonMain/kotlin/io/github/windedge/table/components/Divider.kt
+++ table/src/commonMain/kotlin/io/github/windedge/table/components/Divider.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -16,7 +15,7 @@ import androidx.compose.ui.unit.dp
 fun Divider(
     modifier: Modifier = Modifier,
     thickness: Dp = 1.dp,
-    color: Color = MaterialTheme.colorScheme.outlineVariant
+    color: Color = Color.LightGray
 ) {
     val targetThickness = if (thickness == Dp.Hairline) {
         (1f / LocalDensity.current.density).dp
